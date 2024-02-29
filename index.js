9efd53ebc0e5acced37c124637ab3a69f1ab3ca5
zainab99a <amanhal729@gmail.com>
2024-02-29 20:28:58 +0300

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 function displaySongInfo() {
+    const inputName = document.getElementById('song_name').value;
+    const inputLink = document.getElementById('link').value;
+    const inputdescription = document.getElementById('description').value;
+    const inputYear = document.getElementById('year').value;
 
+    //to convert the link to embeded format
+    const embededFormat=inputLink.replace('/watch?v=', '/embed/')
+
+    document.getElementById('songName').innerText = inputName;
+            document.getElementById('descriptionText').innerText = inputdescription;
+            document.getElementById('date').innerText = inputYear;
+            document.getElementById('songLink').innerHTML = embededFormat;
+            document.getElementById("youtube").src = embededFormat;
+            
+            document.getElementById('formarea').style.display = 'none';
+            document.getElementById('display_area').style.display = 'block';
+
+           
 }
 module.exports = {
     displaySongInfo: displaySongInfo,
-};
+}
